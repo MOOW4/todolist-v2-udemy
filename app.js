f4dd88e25a09029a76ef62cb5656b5871b36d566
MOOW <christianfeliks1@gmail.com>
2023-09-04 20:21:46 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useUnifiedTopology: true });
+const itemsSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item_1 = new Item({
+  name: "Welcome to Your todolist!"
+});
+const item_2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item_3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+const defaultItems = [item_1, item_2, item_3];
+Item.insertMany(defaultItems)
+  .then(() => {
+    console.log("Succesfully saved");
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  //!
+  /*.finally(() => {
+    mongoose.disconnect();
+  }); */
+
+
+/* const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = []; */
+
+app.get("/", function (req, res) {
+	const day = date.getDate();
+
+	res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+	const item = req.body.newItem;
+
+	if (req.body.list === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
+});
+
+app.get("/work", function (req, res) {
+	res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+	res.render("about");
+});
+
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
+});
